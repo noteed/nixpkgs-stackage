e00b2199cd11a569a4c46553a6948a09d05472cd
Vo Minh Thu <thu@hypered.io>
2018-08-25 17:50:03 +0200

    Pin the version of nixpkgs in reesd-stack.
    The chosen version is what I had when I built reesd-stack.nix
    previously.

--- reesd-stack.nix
+++ reesd-stack.nix
@@ -1,5 +1,9 @@
 let
-  pkgs = import <nixpkgs> {
+  nixpkgs = builtins.fetchTarball {
+    sha256 = "0jxn25l8d65h6qnmx9f4dsi2fscqyxc0gvhnkj1bc7kicn9rr5hj";
+    url = https://github.com/nixos/nixpkgs/archive/95a8cb3ade1.tar.gz;
+  };
+  pkgs = import nixpkgs {
     # Provide lts-921-reesd.
     overlays = [ (import ./default.nix) ] ;
   };
