7992ea5e52ffc0d6cee607aa06f9b85822d9c418
Vo Minh Thu <thu@hypered.io>
2018-09-05 06:34:19 +0200

    Update pinned nixpkgs version in reesd-stack.
    The hash if from today, two days after Nix 2.1 was released.

--- reesd-stack.nix
+++ reesd-stack.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = builtins.fetchTarball {
-    sha256 = "0jxn25l8d65h6qnmx9f4dsi2fscqyxc0gvhnkj1bc7kicn9rr5hj";
-    url = https://github.com/nixos/nixpkgs/archive/95a8cb3ade1.tar.gz;
+    sha256 = "1fxmy6fmsx6fp3h2k9jyg4q41dfl1777wswk9bx3v9hzm114pjy7";
+    url = https://github.com/nixos/nixpkgs/archive/e9d4bab04458cd03cb9029d2d571c37cc017ffdc.tar.gz;
   };
   pkgs = import nixpkgs {
     # Provide lts-921-reesd.
@@ -10,7 +10,19 @@ let
   hspkgs = pkgs.haskell.packages.stackage.lts-921-reesd;
 in
 {
+  reesd-stack-lts-921-ghc = hspkgs.ghc;
   reesd-stack-lts-921 = hspkgs.ghcWithPackages (p: with p; [
+    acid-state
+    blaze-builder
+    blaze-html
+    postgresql-simple
+    QuickCheck
+    safecopy
     snap
+    snap-blaze
+    snap-core
+    sqlite-simple
+    tasty
+    tasty-hunit
   ]);
 }
